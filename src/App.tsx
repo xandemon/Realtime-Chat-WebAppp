b9437c996ee455ab08b16b12c5efc94d6b6db42a
deadclown09 <mesandesh.2014@gmail.com>
2023-03-15 22:23:05 +0545

    Authentication of user registration done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainChatApp from "./pages/MainChatApp";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { RouterProvider } from "react-router";
 
 function App() {
   return (
     <>
-      <MainChatApp />
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route index element={<MainChatApp />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
